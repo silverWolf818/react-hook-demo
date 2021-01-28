63b55e3001d6dca9e633cf62b2d82fb4b7946a05
zhouzh <363420113@qq.com>
2021-01-28 14:22:51 +0800

    init

--- src/pages/StateDemo.js
+++ src/pages/StateDemo.js
@@ -1,22 +1,22 @@
-import React, {useState, useEffect,useCallback} from 'react'
+import React, {useState, useEffect} from 'react'
 import {usePreviousValue} from '../hooks/usePreviousValue'
 import {useImmerState} from '../hooks/useImmerState'
 
 const StateDemo = props => {
     const [count, setCount] = useState(0)
-    // const [user, setUser] = useState({
-    //     id: 1,
-    //     name: 'zhangsan',
-    //     address: 'beijing'
-    // })
-
-    const [user,setUser] = useImmerState({
+    const [user, setUser] = useState({
         id: 1,
-        name: 'lisi',
-        address: 'hubei'
+        name: 'zhangsan',
+        address: 'beijing'
     })
 
-    const prev = usePreviousValue(count)
+    // const [user,setUser] = useImmerState({
+    //     id: 1,
+    //     name: 'lisi',
+    //     address: 'hubei'
+    // })
+
+    // const prev = usePreviousValue(count)
 
     useEffect(() => {
         console.log('enter useEffect')
@@ -30,16 +30,16 @@ const StateDemo = props => {
     }
 
     const onChangeUser = () => {
-        // setUser({
-        //     id: 1,
-        //     name: 'zhangsan',
-        //     address: 'beijing'
-        // })
-        setUser(s => {
-            s.id = 2
-            s.name = 'zhangsan'
-            s.address = 'beijing'
+        setUser({
+            id: 1,
+            name: 'zhangsan',
+            address: 'beijing'
         })
+        // setUser(s => {
+        //     s.id = 1
+        //     s.name = 'zhangsan'
+        //     s.address = 'beijing'
+        // })
     }
 
     console.log('render')
@@ -47,7 +47,7 @@ const StateDemo = props => {
     return (
         <div>
             <h2>State Demo</h2>
-            <div>prev: {prev}</div>
+            {/*<div>prev: {prev}</div>*/}
             <div>current: {count}</div>
             <br/>
             <button onClick={onAdd}>add</button>
